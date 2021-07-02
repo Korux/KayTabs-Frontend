6811b6b2ad81ae4593c3b9a45a4943e8361b3bfa
Korux <dongke@oregonstate.edu>
2021-07-02 13:52:27 -0700

    turns out client id is not a secret

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.render(
 <Auth0Provider
     domain="kaytabs.us.auth0.com"
-    clientId={"s"}
+    clientId={"1oduMuPl9UGBxtF1xmIYgwzSZq05EjfH"}
     redirectUri={"http://localhost:3001/editor"}
   >
     <Provider store={store}>
